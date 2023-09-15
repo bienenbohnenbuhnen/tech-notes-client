82b293a639330e389857487110512f3eb5c8f8cb
Andy Cockroft <acockroft1994@gmail.com>
2023-09-15 13:23:27 +0200

    Frontend login auth complete

--- src/App.js
+++ src/App.js
@@ -15,22 +15,27 @@ import Prefetch from "./features/auth/Prefetch";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}></Route>
-      <Route index element={<Public />} />
-      <Route path="login" element={<Login />} />
-      <Route element={<Prefetch />}>
-        <Route path="dash" element={<DashLayout />}>
-          <Route index element={<Welcome />} />
-          <Route path="users">
-            <Route index element={<UsersList />} />
-            <Route path=":id" element={<EditUser />} />
-            <Route path="new" element={<NewUserForm />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Public />} />
+        <Route path="login" element={<Login />} />
+
+        <Route element={<Prefetch />}>
+          <Route path="dash" element={<DashLayout />}>
+            <Route index element={<Welcome />} />
+
+            <Route path="users">
+              <Route index element={<UsersList />} />
+              <Route path=":id" element={<EditUser />} />
+              <Route path="new" element={<NewUserForm />} />
+            </Route>
+
+            <Route path="notes">
+              <Route index element={<NotesList />} />
+              <Route path=":id" element={<EditNote />} />
+              <Route path="new" element={<NewNote />} />
+            </Route>
           </Route>
-          <Route path="notes">
-            <Route index element={<NotesList />} />
-            <Route path=":id" element={<EditNote />} />
-            <Route path="new" element={<NewNote />} />
-          </Route>
+          {/* End Dash */}
         </Route>
       </Route>
     </Routes>
