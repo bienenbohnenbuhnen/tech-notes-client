62b0ea36f492f3c87b0ae42e4c40913c3bbe403d
Andy Cockroft <acockroft1994@gmail.com>
2023-09-18 13:31:32 +0200

    deployment changes

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.js";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 import { store } from "./app/store.js";
 import { Provider } from "react-redux";
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
